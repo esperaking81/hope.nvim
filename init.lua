349e6ae356bf88baee1619542d6e24432dd99c40
= <=>
2024-06-23 20:24:17 +0100

    feat: add git integration

--- init.lua
+++ init.lua
@@ -157,6 +157,9 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- Use tab to switch pane in normal mode
+vim.keymap.set('n', '<Tab>', '<cmd>tabnext<cr>', { desc = 'Switch tabs' })
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -783,16 +786,17 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'projekt0n/github-nvim-theme',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.background 'light'
+      vim.cmd.colorscheme 'github_light_default'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 
--- lua/custom/plugins/flutter.lua
+++ lua/custom/plugins/flutter.lua
@@ -1,6 +1,7 @@
 return {
   'akinsho/flutter-tools.nvim',
   lazy = false,
+  ft = 'dart',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'stevearc/dressing.nvim',
@@ -27,15 +28,19 @@ return {
       dev_log = {
         enabled = true,
         notify_errors = false,
-        open_cmd = '20new',
+        open_cmd = '10new',
+      },
+      outline = {
+        open_cmd = '30vnew', -- command to use to open the outline buffer
+        auto_open = true, -- if true this will open the outline automatically when it is first populated
       },
     }
 
     vim.keymap.set('n', '<leader>m', require('telescope').extensions.flutter.commands, { desc = 'Open command Flutter' })
     vim.keymap.set('n', '<leader>br', function()
-      vim.cmd '20new'
-      vim.cmd 'te fvm flutter packages pub run build_runner build --delete-conflicting-outputs'
+      vim.cmd '10new'
+      vim.cmd 'te dart run build_runner build --delete-conflicting-outputs'
       vim.cmd '2sleep | normal G'
-    end)
+    end, { desc = 'Run build_runner' })
   end,
 }
